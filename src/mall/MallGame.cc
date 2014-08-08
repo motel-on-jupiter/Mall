fd749ae0fc212888309eb575eda6a145c9740221
bobozeta <bobo_zeta@motel-on-jupiter.sakuraweb.com>
2014-08-08 21:15:19 +0900

    制御を調整する

--- src/mall/MallGame.cc
+++ src/mall/MallGame.cc
@@ -240,25 +240,27 @@ int MallGame::Draw(glm::vec2 window_size) {
 }
 
 int MallGame::OnKeyboardDown(SDL_Keycode key, glm::vec2 window_size) {
-  if ((gamecase_ == nullptr) &&
-      ((key == SDLK_1) || (key == SDLK_2))) {
-    LOGGER.Info("Set up the game case");
-    if (key == SDLK_1) {
-      gamecase_ = new GridWalkingCase();
-    } else {
-      gamecase_ = new BridgeTrafficCase();
+  if (gamecase_ == nullptr) {
+    if ((key == SDLK_1) || (key == SDLK_2)) {
+      LOGGER.Info("Set up the game case");
+      if (key == SDLK_1) {
+        gamecase_ = new GridWalkingCase();
+      } else {
+        gamecase_ = new BridgeTrafficCase();
+      }
+      int ret = gamecase_->Initialize(window_size);
+      if (ret < 0) {
+        LOGGER.Error("Failed to initialize the game case");
+        return -1;
+      }
     }
-    int ret = gamecase_->Initialize(window_size);
-    if (ret < 0) {
-      LOGGER.Error("Failed to initialize the game case");
-      return -1;
+  } else {
+    if (key == SDLK_0) {
+      LOGGER.Info("Clean up the game-case");
+      gamecase_->Finalize();
+      delete gamecase_;
+      gamecase_ = nullptr;
     }
-  }
-  if ((gamecase_ != nullptr) && (key == SDLK_0)) {
-    LOGGER.Info("Clean up the game-case");
-    gamecase_->Finalize();
-    delete gamecase_;
-    gamecase_ = nullptr;
   }
   return 0;
 }
