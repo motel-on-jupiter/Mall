99de53d8cc1d3fb8793c72cefec4021d188907bb
bobozeta <bobo_zeta@motel-on-jupiter.sakuraweb.com>
2014-08-16 15:02:04 +0900

    Mallプロジェクトのフォーマットに直す

--- src/util/system/FPSCounter.cc
+++ src/util/system/FPSCounter.cc
@@ -1,56 +1,24 @@
 /**
  * Copyright (C) 2014 The Motel on Jupiter
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 #include "FPSCounter.h"
 
-FPSCounter::FPSCounter(int intervaltime)
-    : intervaltime_(intervaltime),
-      count_(0),
-      countstarttime_(0),
-      fps_(0) {
+FPSCounter::FPSCounter(int sampling_msec)
+: sampling_msec_(sampling_msec),
+  fps_(0),
+  count_(0),
+  count_start_msec(0) {
 }
 
-FPSCounter::~FPSCounter() {
-}
-
-int FPSCounter::Update(int nowtime) {
-  // update_interval_が0以下の時は更新しない
-  if (0 >= intervaltime_) {
-    return 1;
-  }
-
-  // フレーム数をカウント
+void FPSCounter::Update(int current_msec) {
+  // Count frames
   ++count_;
 
-  //
-  int elapsedtime = nowtime - countstarttime_;
-  if (elapsedtime > intervaltime_) {
-    //計測
-    fps_ = (intervaltime_ * count_ / elapsedtime) / (intervaltime_ / 1000);
-
-    //初期化
-    countstarttime_ = nowtime;
+  // Update FPS
+  int elapsed_msec = current_msec - count_start_msec;
+  if (elapsed_msec > sampling_msec_) {
+    fps_ = (sampling_msec_ * count_ / elapsed_msec) / (sampling_msec_ / 1000);
     count_ = 0;
+    count_start_msec = current_msec;
   }
-
-  return 0;
 }
-
--- src/util/system/FPSCounter.h
+++ src/util/system/FPSCounter.h
@@ -1,49 +1,24 @@
 /**
  * Copyright (C) 2014 The Motel on Jupiter
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
-#ifndef WATERSURFACESIM_FPSCOUNTER_H_
-#define WATERSURFACESIM_FPSCOUNTER_H_
+#ifndef FPSCOUNTER_H_
+#define FPSCOUNTER_H_
 
 class FPSCounter {
  public:
-  FPSCounter(int intervaltime);
-  virtual ~FPSCounter();
+  FPSCounter(int sampling_msec);
+  ~FPSCounter() {}
 
-  int Update(int nowtime);
+  void Update(int current_msec);
 
-  // accessor
-  inline int fps() const {
-    return fps_;
-  }
-
-  // mutator
-  inline void set_update_interval(int time) {
-    intervaltime_ = time;
-  }
+  int sampling_msec() const { return sampling_msec_; }
+  int fps() const { return fps_; }
 
  private:
-  int intervaltime_;
-  int count_;
-  int countstarttime_;
+  int sampling_msec_;
   int fps_;
+  int count_;
+  int count_start_msec;
 };
 
-#endif /* WATERSURFACESIM_FPSCOUNTER_H_ */
+#endif /* FPSCOUNTER_H_ */
