e2760f19f79f65e1e14ba531a270f8db881de548
bobozeta <bobo_zeta@motel-on-jupiter.sakuraweb.com>
2014-08-17 19:12:45 +0900

    行列スタックを使って最適化を行う

--- src/entity/EntityDraw.cc
+++ src/entity/EntityDraw.cc
@@ -22,11 +22,14 @@ EntityTriangleDraw::EntityTriangleDraw(BaseEntity &entity, bool fill) :
 }
 
 void EntityTriangleDraw::Draw() {
+  glPushMatrix();
+  glMultMatrixf(glm::value_ptr(glm::translate(glm::vec3(entity().pos(), 0.0f))));
   glBegin(fill_ ? GL_TRIANGLES : GL_LINE_LOOP);
-  glVertex2fv(glm::value_ptr(entity().pos() + glm::rotate(entity().scale() * glm::vec2(0.0f, -0.5f), entity().rot())));
-  glVertex2fv(glm::value_ptr(entity().pos() + glm::rotate(glm::vec2(entity().scale() * glm::vec2(-0.5f, 0.5f)), entity().rot())));
-  glVertex2fv(glm::value_ptr(entity().pos() + glm::rotate(glm::vec2(entity().scale() * glm::vec2(0.5f, 0.5f)), entity().rot())));
+  glVertex2fv(glm::value_ptr(glm::rotate(entity().scale() * glm::vec2(0.0f, -0.5f), entity().rot())));
+  glVertex2fv(glm::value_ptr(glm::rotate(glm::vec2(entity().scale() * glm::vec2(-0.5f, 0.5f)), entity().rot())));
+  glVertex2fv(glm::value_ptr(glm::rotate(glm::vec2(entity().scale() * glm::vec2(0.5f, 0.5f)), entity().rot())));
   glEnd();
+  glPopMatrix();
 }
 
 EntityRectangleDraw::EntityRectangleDraw(BaseEntity &entity, bool fill) :
@@ -34,10 +37,13 @@ EntityRectangleDraw::EntityRectangleDraw(BaseEntity &entity, bool fill) :
 }
 
 void EntityRectangleDraw::Draw() {
+  glPushMatrix();
+  glMultMatrixf(glm::value_ptr(glm::translate(glm::vec3(entity().pos(), 0.0f))));
   glBegin(fill_ ? GL_QUADS : GL_LINE_LOOP);
-  glVertex2fv(glm::value_ptr(entity().pos() + glm::rotate(entity().scale() * -0.5f, entity().rot())));
-  glVertex2fv(glm::value_ptr(entity().pos() + glm::rotate(entity().scale() * glm::vec2(-0.5f, 0.5f), entity().rot())));
-  glVertex2fv(glm::value_ptr(entity().pos() + glm::rotate(entity().scale() * 0.5f, entity().rot())));
-  glVertex2fv(glm::value_ptr(entity().pos() + glm::rotate(entity().scale() * glm::vec2(0.5f, -0.5f), entity().rot())));
+  glVertex2fv(glm::value_ptr(glm::rotate(entity().scale() * -0.5f, entity().rot())));
+  glVertex2fv(glm::value_ptr(glm::rotate(entity().scale() * glm::vec2(-0.5f, 0.5f), entity().rot())));
+  glVertex2fv(glm::value_ptr(glm::rotate(entity().scale() * 0.5f, entity().rot())));
+  glVertex2fv(glm::value_ptr(glm::rotate(entity().scale() * glm::vec2(0.5f, -0.5f), entity().rot())));
   glEnd();
+  glPopMatrix();
 }
